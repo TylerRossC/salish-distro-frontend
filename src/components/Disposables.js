b59c32971f09c237c1db5583c6a623d397eb195c
tyler <tylercharawell@gmail.com>
2022-08-08 21:20:46 -0700

    Rendering ProductCards in Disposables.js and Hardware.js with styling added in Disposables.css and Hardware.css

--- src/components/Disposables.js
+++ src/components/Disposables.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import ProductCards from './ProductCards'
+import "./Ejuice.css"
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 const Disposables = () => {
   return (
     <div className="disposables">
     Disposables
+    <div className="product-cards">
+    <ProductCards/>
+    </div>
+    <div className="pagination">
+    <Stack spacing={2}>
+      <Pagination count={10} color="primary" />
+    </Stack>
+    </div>
     </div>
   )
 }
